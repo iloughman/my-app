742802bde6d8a819987aee749d4720f7bafc51a4
iloughman <iloughman@gmail.com>
2019-02-18 13:07:17 -0500

    Added ExchangeRatesMenuList component to handle selection of from rate and to rate

--- src/js/components/ExchangeRates.jsx
+++ src/js/components/ExchangeRates.jsx
@@ -1,35 +1,45 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { getExchangeRates } from "../actions/index";
+import ExchangeRatesMenuList from "./ExchangeRatesMenuList";
 
 export class ExchangeRates extends Component {
     constructor() {
         super();
         this.state = {
-            exchangeRates: {},
-            title: "The title"
+            exchangeRates: [],
+            fromRate: {},
+            toRate: {},
+            anchorEl: null
         };
-
     }
+
     componentDidMount() {
         this.props.getExchangeRates().then(res => {
-            console.log("res", res);
-            this.setState({exchangeRates: res.rates});
+            this.setState({exchangeRates: Object.entries(res.rates).map(entry => {
+                    return {rate: entry[1], countryCode: entry[0]};
+                }
+            )});
         });
     }
+
+    setFromRate = (rate) => {
+        this.setState({fromRate: rate});
+    }
+
+    setToRate = (rate) => {
+        this.setState({toRate: rate});
+    }
+
     render() {
         return (
             <div>
-                <h3>{this.state.title}</h3>
-                <ul>
-                    {Object.keys(this.state.exchangeRates).map(el => (
-                        <li key={this.state.exchangeRates}>
-                            {el}
-                        </li>
-                    ))}
-                </ul>
+                <div>Convert from : {this.state.fromRate.countryCode}</div>
+                <ExchangeRatesMenuList exchangeRates={this.state.exchangeRates} setRate={this.setFromRate}/>
+                <div> to: {this.state.toRate.countryCode} </div>
+                <ExchangeRatesMenuList exchangeRates={this.state.exchangeRates} setRate={this.setToRate} />
             </div>
-        )
+        );
     }
 }
 
---
+++ src/js/components/ExchangeRatesMenuList.jsx
@@ -0,0 +1,62 @@
+import React, { Component } from "react";
+import Button from '@material-ui/core/Button';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import MenuList from '@material-ui/core/MenuList';
+
+export class ExchangeRatesMenuList extends Component {
+    constructor() {
+        super();
+        this.state = {
+            title: "The title",
+            anchorEl: null,
+            rate: {}
+        };
+        this.handleClick = this.handleClick.bind(this);
+        this.handleClose = this.handleClose.bind(this);
+    }
+
+    handleClick = event => {
+        this.setState({ anchorEl: event.currentTarget });
+    }
+
+    handleClose = (el) => {
+        this.setState({ anchorEl: null });
+        this.setState({ rate: el });
+        this.props.setRate(el);
+    }
+
+    closeList = () => {
+        this.setState({anchorEl: null});
+    }
+
+    render() {
+        const { anchorEl } = this.state;
+
+        return (
+            <div>
+                <Button
+                    aria-owns={anchorEl ? 'simple-menu' : undefined}
+                    aria-haspopup="true"
+                    onClick={this.handleClick}
+                >
+                    {this.state.rate.countryCode || "Select a country"}
+                </Button>
+                <Menu
+                    id="simple-menu"
+                    anchorEl={anchorEl}
+                    open={Boolean(anchorEl)}
+                    onClick={this.closeList}
+                >
+                    <MenuList>
+                        {this.props.exchangeRates.map((el, index) => {
+                            return <MenuItem onClick={() => this.handleClose(el)} key={index}>{el.countryCode}</MenuItem>
+                        })}
+                    </MenuList>
+                </Menu>
+            </div>
+        );
+    }
+}
+
+export default ExchangeRatesMenuList;
